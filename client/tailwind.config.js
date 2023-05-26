be2091b57ee7cfd833263b6bdf74f97306059e92
Augustus Chong <112513956+AugustusChong@users.noreply.github.com>
2023-05-25 23:29:21 -0700

    Extended tailwind color theme with fontPrimary and fontSecondary colors

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,16 +4,20 @@ export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      screens: {
-        xs: "480px",
-      },
-      fontFamily: {
-        inter: ["Inter var", "sans-serif"],
-      },
       boxShadow: {
         card: "0 0 1px 0 rgba(189,192,207,0.06),0 10px 16px -1px rgba(189,192,207,0.2)",
         cardHover:
           "0 0 1px 0 rgba(189,192,207,0.06),0 10px 16px -1px rgba(189,192,207,0.4)",
+      },
+      colors: {
+        fontPrimary: "#222328",
+        fontSecondary: "#666e75",
+      },
+      fontFamily: {
+        inter: ["Inter var", "sans-serif"],
+      },
+      screens: {
+        xs: "480px",
       },
     },
   },
