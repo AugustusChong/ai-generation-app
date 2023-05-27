7016506896b052713ebe8b5272adca7c38bb948a
Augustus Chong <112513956+AugustusChong@users.noreply.github.com>
2023-05-27 02:22:27 -0700

    Moved routes to MainContent component and added image showcase path

---
+++ client/src/components/MainContent.tsx
@@ -0,0 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+import { Home, ImageShowcase, CreatePost } from "../pages";
+
+const MainContent = () => {
+  return (
+    <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/image-showcase" element={<ImageShowcase />} />
+        <Route path="/create-post" element={<CreatePost />} />
+      </Routes>
+    </main>
+  );
+};
+
+export default MainContent;
