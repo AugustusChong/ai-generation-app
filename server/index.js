8d033365cf54fa8051fe91f7410bd87e2e6d50b4
Augustus Chong <112513956+AugustusChong@users.noreply.github.com>
2023-06-02 17:46:58 -0700

    Setup DALL-E route and skeleton for post route

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./mongodb/connect.js";
+import postRoutes from "./routes/postRoutes.js";
+import dallERoutes from "./routes/dallERoutes.js";
 
 dotenv.config();
 
@@ -9,9 +11,8 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 
-app.get("/", async (req, res) => {
-  res.send("Hello from DALL-E");
-});
+app.use("/api/v1/post", postRoutes);
+app.use("/api/v1/dallE", dallERoutes);
 
 const startServer = async () => {
   try {
