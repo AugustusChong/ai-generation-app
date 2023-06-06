33ec260e04698f67ad88dacece1929f4da99be24
Augustus Chong <112513956+AugustusChong@users.noreply.github.com>
2023-06-06 16:54:40 -0700

    Util function to get random prompt

---
+++ client/src/utils/index.ts
@@ -0,0 +1,13 @@
+import { surpriseMePrompts } from "../constants";
+
+export function getRandomPrompt(prompt: string): string {
+  const randomIndex: number = Math.floor(
+    Math.random() * surpriseMePrompts.length
+  );
+  const randomPrompt: string = surpriseMePrompts[randomIndex];
+
+  if (randomPrompt === prompt) {
+    return getRandomPrompt(prompt);
+  }
+  return randomPrompt;
+}
