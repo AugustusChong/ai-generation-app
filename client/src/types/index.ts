13b1306587e76a2b0d5a4aac093079a19caa91f5
Augustus Chong <112513956+AugustusChong@users.noreply.github.com>
2023-06-08 17:12:55 -0700

    Merged components and pages types into index to make it less confusing

--- client/src/types/index.ts
+++ client/src/types/index.ts
@@ -1,2 +1,21 @@
-export type { FormFieldProps } from "./components";
-export type { Form } from "./pages";
+export type Form = {
+  name: string;
+  prompt: string;
+  photo: string;
+};
+
+export type FormFieldProps = {
+  labelName?: string;
+  type?: string;
+  name?: string;
+  placeholder?: string;
+  value?: string;
+  handleChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  isSurpriseMe?: boolean;
+  handleSurpriseMe?: () => void;
+};
+
+export type RenderCardProps = {
+  data: any;
+  title: string;
+};
--- client/src/types/pages.ts
+++
@@ -1,5 +0,0 @@
-export type Form = {
-  name: string;
-  prompt: string;
-  photo: string;
-};
