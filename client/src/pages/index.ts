4bd841f10e0f65b9fb1eb7e0b332e4177aedd9ff
Augustus Chong <112513956+AugustusChong@users.noreply.github.com>
2023-06-23 21:21:43 -0700

    Added GPTChat component to pages index exports

--- client/src/pages/index.ts
+++ client/src/pages/index.ts
@@ -1,5 +1,6 @@
 import Home from "./Home";
 import ImageShowcase from "./ImageShowcase";
 import CreatePost from "./CreatePost";
+import GPTChat from "./GPTChat";
 
-export { Home, ImageShowcase, CreatePost };
+export { Home, ImageShowcase, CreatePost, GPTChat };
