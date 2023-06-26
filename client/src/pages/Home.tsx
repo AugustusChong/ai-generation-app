1a896ab50e54d303d228366c69396e8f65b42662
Augustus Chong <112513956+AugustusChong@users.noreply.github.com>
2023-06-26 15:34:14 -0700

    Added link to GPT Chat on Home

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -2,12 +2,20 @@ import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <Link
-      to="/image-showcase"
-      className="font-inter font-medium bg-buttonPrimary text-white px-4 py-2 rounded-md"
-    >
-      Image Showcase
-    </Link>
+    <>
+      <Link
+        to="/image-showcase"
+        className="font-inter font-medium bg-buttonPrimary text-white px-4 py-2 mr-4 rounded-md"
+      >
+        Image Showcase
+      </Link>
+      <Link
+        to="/gpt-chat"
+        className="font-inter font-medium bg-buttonPrimary text-white px-4 py-2 rounded-md"
+      >
+        GPT Chat
+      </Link>
+    </>
   );
 };
 
