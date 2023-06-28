e9ececd8628c1b47bdb9532a541d7a3a0f5333f0
Augustus Chong <112513956+AugustusChong@users.noreply.github.com>
2023-06-27 17:37:23 -0700

    Added sectionClassNames to constants

--- client/src/constants/index.ts
+++ client/src/constants/index.ts
@@ -46,3 +46,6 @@ export const surpriseMePrompts = [
   "A man wanders through the rainy streets of Tokyo, with bright neon signs, 50mm",
   "A Space Shuttle flying above Cape Town, digital art",
 ];
+
+export const sectionClassNames =
+  "sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]";
